914998d7b9957bd52276de0711576bf0892f5c6b
rubinskii88 <rubinsky@yandex.ru>
2025-04-18 17:25:34 +0300

    started regex router development

--- index.php
+++ index.php
@@ -1,5 +1,7 @@
 <?php
 
+require __DIR__ . '/config/debug.php';
+
 $path = $_SERVER['REQUEST_URI'];
 $path = parse_url($path, PHP_URL_PATH);
 
@@ -16,9 +18,10 @@ spl_autoload_register('autoload');
 // require 'src/Router.php';
 $router = new Framework\Router;
 
-$router->add('/home/index', ['controller' => 'Home', 'action' => 'index']);
-$router->add('/products', ['controller' => 'Products', 'action' => 'index']);
-$router->add('/', ['controller' => 'Home', 'action' => 'index']);
+$router->add('/{controller}/{action}');
+// $router->add('/home/index', ['controller' => 'Home', 'action' => 'index']);
+// $router->add('/products', ['controller' => 'Products', 'action' => 'index']);
+// $router->add('/', ['controller' => 'Home', 'action' => 'index']);
 
 $params = $router->match($path);
 
--- src/Framework/Router.php
+++ src/Framework/Router.php
@@ -7,7 +7,7 @@ class Router
 
   private array $routes = [];
 
-  public function add(string $path, array $params): void
+  public function add(string $path, array $params = []): void
   {
     $this->routes[] = [
       'path' => $path,
@@ -17,12 +17,49 @@ class Router
 
   public function match(string $path): array|bool
   {
+
     foreach ($this->routes as $route) {
-      if ($route['path'] === $path) {
-        return $route['params'];
+      $pattern = '#^/(?<controller>[a-z]+)/(?<action>[a-z]+)$#';
+
+      echo '<pre>';
+
+      d($pattern);
+      d($route['path']);
+      
+
+
+      $this->getPatternFromRoutePath($route['path']);
+
+      if (preg_match($pattern, $path, $matches)) {
+
+        $matches = array_filter($matches, 'is_string', ARRAY_FILTER_USE_KEY);
+
+        return $matches;
       }
     }
-    
+
     return false;
+  }
+
+  private function getPatternFromRoutePath(string $routePath)
+  {
+
+    $routePath = trim($routePath, '/');
+
+    $segments = explode('/', $routePath);
+
+    $segments = array_map(
+      function (string $segment): string {
+        
+        preg_match('#^\{([a-z][a-z0-9]*)\}$#', $segment, $matches);
+        
+        $segment = '(?<' . $matches[1] . '>[a-z]+)';
+        // d($matches);
+        return $segment;
+      },
+      $segments
+    );
+
+    d($segments);
   }
 }
